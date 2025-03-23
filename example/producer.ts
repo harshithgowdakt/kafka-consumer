b36ed1f8f02c220d1bf8c25e4afe6c2b224c0b85
Harshith Gowda <harshithgowdakt@gmail.com>
2025-03-23 16:39:30 +0530

    added producer for producing messages

---
+++ example/producer.ts
@@ -0,0 +1,74 @@
+import { Kafka, Producer, Message } from "kafkajs";
+
+interface AppLog {
+    timestamp: string;
+    lvl: string;
+    msg: string;
+    appName: string;
+    hostname: string;
+    env: string;
+}
+
+// Kafka setup
+const kafka = new Kafka({
+    brokers: ["localhost:29092"],
+});
+
+const producer: Producer = kafka.producer();
+
+async function publishCsvToKafka(topic: string): Promise<void> {
+    await producer.connect();
+    console.log("Kafka Producer connected");
+
+    const batchSize = 1000;
+    let batch: Message[] = [];
+    let count = 0;
+    const startTime = new Date();
+
+    try {
+        for (let index = 0; index < 1; index++) {
+            const eventTime = new Date(
+                startTime.getTime() + index * 1000,
+            ).toISOString();
+
+            const formattedRow: AppLog = {
+                timestamp: eventTime,
+                lvl: "INFO",
+                msg: `Event ${index} occurred.`,
+                appName: "local-app",
+                hostname: "localhost",
+                env: "local",
+            };
+
+            batch.push({ value: JSON.stringify(formattedRow) });
+
+            if (batch.length === batchSize) {
+                await producer.send({ topic, messages: batch });
+                count += batch.length;
+                console.log(
+                    `Published batch of ${batch.length} events. Total published: ${count}`,
+                );
+                batch = [];
+            }
+        }
+
+        if (batch.length > 0) {
+            await producer.send({ topic, messages: batch });
+            count += batch.length;
+            console.log(
+                `Published final batch of ${batch.length} events. Total published: ${count}`,
+            );
+        }
+    } catch (error) {
+        console.error("Error publishing batch to Kafka:", error);
+    } finally {
+        await producer.disconnect();
+        console.log("Kafka Producer disconnected");
+    }
+}
+
+const topic = "local-app-logs";
+
+publishCsvToKafka(topic).then(() => {
+    console.log("Publishing completed successfully.");
+});
